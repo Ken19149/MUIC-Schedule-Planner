3f1037e4be46dd4c1df212be8fd0b66bf485b076
Ken980 <kenboy9800@gmail.com>
2025-01-19 04:07:26 +0700

    aasds

---
+++ scheduleplanner/Test.java
@@ -0,0 +1,10 @@
+package scheduleplanner;
+
+import org.json.JSONObject;
+
+public class Test {
+    public static void main(String args[]){
+       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
+       System.out.println(jo);
+    }
+}
