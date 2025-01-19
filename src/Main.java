b8653f598714ac0b6b75689b8cb85b52bcac024e
Ken980 <kenboy9800@gmail.com>
2025-01-19 17:55:53 +0700

    initial setup

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         try{
-            JSONObject json = new JSONObject(IOUtils.toString(new URL("https://os.muic.io/data.json"), Charset.forName("UTF-8")));
+            JSONObject json = new JSONObject(IOUtils.toString(new URL("https://os.muic.io/data.json"), StandardCharsets.UTF_8));
             //System.out.println(json);
 
             /*
