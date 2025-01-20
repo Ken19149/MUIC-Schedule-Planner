1346010add4c72be5118f2e4047e71e3823dc32a
Ken980 <kenboy9800@gmail.com>
2025-01-20 20:12:16 +0700

    add input from text file

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,64 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+    start:
+    try create -> catch already exist -> read
+
+*/
+
 public class Main {
+
+    private static final String fileLocation = "src/schedule.txt";
+    private static final String instructionMessage = "type help for information";
+
     public static void main(String[] args){
+        String initialData = initialFile();
+        System.out.println("initial data: " + initialData);
+    }
+
+    public static JSONObject muicData(){
         try{
-            JSONObject json = new JSONObject(IOUtils.toString(new URL("https://os.muic.io/data.json"), StandardCharsets.UTF_8));
-            //System.out.println(json);
-
-            System.out.println(json.getString("title"));
-            System.out.println(json.getJSONArray("data").getJSONArray(0));
-
+            URL link = new URL("https://os.muic.io/data.json");
+            JSONObject json = new JSONObject(IOUtils.toString(link, StandardCharsets.UTF_8));
+            return json;
         }
         catch (Exception e){
             e.printStackTrace();
         }
+        return null;
     }
+
+    public static String initialFile(){
+        // try to create schedule.txt with initial message but if it's already exist, return data from that file
+        try {
+            File file = new File(fileLocation);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+                FileWriter myWriter = new FileWriter(fileLocation);
+                myWriter.write(instructionMessage);
+                myWriter.close();
+            } else {
+                Scanner myReader = new Scanner(file);
+                String data = "";
+                while (myReader.hasNextLine()) {
+                    String line = myReader.nextLine();
+                    data += line + "\n";
+                }
+                myReader.close();
+                return data;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
